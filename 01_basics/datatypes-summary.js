77613c8666025bf25e58ce44f114b23c15753f4b
Shahzad <111357352+Shahzadsdr55@users.noreply.github.com>
2024-11-19 13:23:53 +0000

    updates

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console(id == anotherId);
+
+const bigNumber = 34321443145398765n 
+
+// Reference (Non premitive)
+
+// Array, Objects, Functions
+
+
+
+const heroes =["Shahzad","Shahbaz","Shahnawaz"]
+let myObj = {
+    name: "Shahzad",
+    age: 26,
+}
+
+const myFunction = function(){
+    console.log("Hii");
+}
+
+console.log(typeof anotherId);
